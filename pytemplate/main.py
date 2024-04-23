cba17e5fdbd3100a1c323caba94d0bc40b53efe8
mlk621-qtm <116674166+mlk621-qtm@users.noreply.github.com>
2024-04-23 13:18:29 -0500

    initial repo setup
    initial repo setup

--- pytemplate/main.py
+++
@@ -1,9 +0,0 @@
-"""This is the main module of the pytemplate package."""
-
-
-def hello_world() -> str:
-    """Print 'Hello, world!' to the console."""
-    return "Hello, World!"
-
-
-print(hello_world())
